7a767b018774c971b8e40177aca3045c7341cda7
tqer39 <tqer39@gmail.com>
2022-01-28 08:37:59 +0900

    shellcheckに対応

--- setup.sh
+++ setup.sh
@@ -2,17 +2,17 @@
 
 set -eu
 
-_REPOSITORY=https://github.com/tqer39/dotfiles
+REPOSITORY=https://github.com/tqer39/dotfiles
 
 main() {
   detect_os
-  if [ "$_PLATFORM" == 'linux' -a "$_PLATFORM" == 'mac' ]; then
+  if [ "$PLATFORM" == 'linux' ] && [ "$PLATFORM" == 'mac' ]; then
     detect_distribution
   fi
 
-  if [ "$_PLATFORM" == 'linux' ]; then
+  if [ "$PLATFORM" == 'linux' ]; then
     install_linux
-  elif [ "$_PLATFORM" == 'mac' ]; then
+  elif [ "$PLATFORM" == 'mac' ]; then
     install_mac
   else
     abort 'このOSは対応していません'
@@ -28,39 +28,14 @@ abort() {
 
 detect_os() {
   if [ "$(uname)" == "Darwin" ]; then
-    _PLATFORM=mac
-  elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
-    _PLATFORM=windows
-  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    _PLATFORM=linux
+    PLATFORM=mac
+  elif [ "$(uname -s)" == "MINGW" ]; then
+    PLATFORM=windows
+  elif [ "$(uname -s)" == "Linux" ]; then
+    PLATFORM=linux
   else
-    _PLATFORM="Unknown OS"
+    PLATFORM="Unknown OS"
     abort "Your platform ($(uname -a)) is not supported."
-  fi
-}
-
-detect_distribution() {
-  if [ -e /etc/lsb-release ]; then
-    _DISTRIBUTION=ubuntu
-    _DISTRIBUTION_VERSION=$(cat /etc/os-release | grep UBUNTU_CODENAME= | cut -c 17-)
-    echo $_DISTRIBUTION
-    echo $_DISTRIBUTION_VERSION
-  elif [ -e /etc/debian_version ] || [ -e /etc/debian_release ]; then
-    _DISTRIBUTION=debian
-  elif [ -e /etc/redhat-release ]; then
-    if [ -e /etc/oracle-release ]; then
-      _DISTRIBUTION=oracle
-    else
-      _DISTRIBUTION=redhat
-    fi
-  elif [ -e /etc/fedora-release ]; then
-    _DISTRIBUTION=fedora
-  elif [ -e /etc/arch-release ]; then
-    _DISTRIBUTION=arch
-  else
-    echo "Your distributio is not supported."
-    _DISTRIBUTION="Unknown Distribution"
-    exit 1
   fi
 }
 
@@ -78,24 +53,26 @@ is_exists() {
 }
 
 setup_dotfiles() {
-  if [ ! -e $HOME/dotfiles ]; then
+  if [ ! -e "$HOME/dotfiles" ]; then
     if is_exists "git"; then
-      git clone "${_REPOSITORY}.git" $HOME/dotfiles
+      git clone "${REPOSITORY}.git" "$HOME/dotfiles"
     else
-      cd $HOME/Downloads
-      wget -O ./dotfiles.zip "${_REPOSITORY}/archive/main.zip"
+      cd "$HOME/Downloads"
+      wget -O ./dotfiles.zip "${REPOSITORY}/archive/main.zip"
       unzip ./dotfiles.zip
       rm -rf ./dotfiles.zip
-      mv ./dotfiles-main $HOME/dotfiles
+      mv ./dotfiles-main "$HOME/dotfiles"
       cd
     fi
   fi
 
-  DOTFILES=$(ls ./target -a1 | grep -v "\.$")
+  mkdir -p ./tmp
+  find ./target -type f -name '.*' > ./tmp/dotfiles.txt
+  sed -i.org "s/\.\/target\///g" ./tmp/dotfiles.txt
+  mapfile -t DOTFILES < ./tmp/dotfiles.txt
 
-  for f in ${DOTFILES[@]}; do
-    echo "ln -sf $HOME/dotfiles/$f $HOME/$f"
-    ln -sf $HOME/dotfiles/target/$f $HOME/$f
+  for f in "${DOTFILES[@]}"; do
+    ln -sf "$HOME/dotfiles/target/$f" "$HOME/$f"
   done
 }
 
